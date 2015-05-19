818d4eb6941e3e9eef5a5a25212e41edfa9a4380
jemacom <jemacomchokri@gmail.com>
2015-05-19 22:48:27 +0100

    adding two thread

---
+++ semaphore.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+
+
+void myfunc1( void *ptr );
+void myfunc2( void *ptr );
+
+
+int main(int argc , char *argv[])
+{
+    pthread_t thread1;
+    pthread_t thread2;
+
+    char *msg1 = "Thread 1";
+    char *msg2 = "Thread 2";
+    pthread_create (&thread1 , NULL, (void *) &myfunc1 , (void *) msg1);
+    pthread_create (&thread2 , NULL, (void *) &myfunc2 , (void *) msg2);
+
+    pthread_join(thread1 , NULL);
+    pthread_join(thread2 , NULL);
+
+    return 0;
+}
+
+void myfunc1( void *ptr)
+{
+    char *msg = (char *) ptr;
+    printf("%s \n" , msg);
+    pthread_exit(0);
+}
+
+void myfunc2( void *ptr )
+{
+    char *msg = (char *) ptr;
+    printf("%s \n" , msg);
+    pthread_exit(0);
+}
