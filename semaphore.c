6bb93ba5e56a0f58154370e11877dc8293bad02a
jemacom <jemacomchokri@gmail.com>
2015-05-19 22:59:18 +0100

    Adding a semaphore

--- semaphore.c
+++ semaphore.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 void myfunc1( void *ptr );
 void myfunc2( void *ptr );
 char buf[24];
+sem_t mutex;
 
 int main(int argc , char *argv[])
 {
@@ -12,12 +14,15 @@ int main(int argc , char *argv[])
 
     char *msg1 = "Thread 1";
     char *msg2 = "Thread 2";
+    sem_init(&mutex , 0,1);
+
     pthread_create (&thread1 , NULL, (void *) &myfunc1 , (void *) msg1);
     pthread_create (&thread2 , NULL, (void *) &myfunc2 , (void *) msg2);
 
     pthread_join(thread1 , NULL);
     pthread_join(thread2 , NULL);
 
+    sem_destroy(&mutex);
     return 0;
 }
 
@@ -25,7 +30,11 @@ void myfunc1( void *ptr)
 {
     char *msg = (char *) ptr;
     printf("%s \n" , msg);
+
+    sem_wait(&mutex);
     sprintf(buf , "%s" , "Salem ");
+    sem_post(&mutex)
+
     pthread_exit(0);
 }
 
@@ -33,6 +42,10 @@ void myfunc2( void *ptr )
 {
     char *msg = (char *) ptr;
     printf("%s \n" , msg);
+
+    sem_wait(&mutex);
     printf("%s\n", buf);
+    sem_post(&mutex);
+
     pthread_exit(0);
 }
